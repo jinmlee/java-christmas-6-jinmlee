d773b20ca866ceabfb6acfa32a627f1ce9e04082
이진모 <jinmlee210@naver.com>
2023-11-09 20:38:12 +0900

    feat: 방문 날짜를 입력받는 기능 구현

---
+++ src/main/java/christmas/controller/ChristmasEvent.java
@@ -0,0 +1,9 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+
+public class ChristmasEvent {
+    public void startOrder() {
+        InputView.readDate();
+    }
+}
---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,17 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static void readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        while (true) {
+            try {
+                String input = Console.readLine();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
