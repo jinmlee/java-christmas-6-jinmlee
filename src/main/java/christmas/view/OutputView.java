8bb3de562039800d0698b556fa3b74bc779bcbd2
이진모 <jinmlee210@naver.com>
2023-11-13 17:33:05 +0900

    refactor: 출력에서 숫자 마이너스를 출력하는 부분 로직 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,17 +6,17 @@ import christmas.enums.Menu;
 import java.util.HashMap;
 
 public class OutputView {
+    private final int MINUS = -1;
     public void printOrderMenu(HashMap<Menu, Integer> orderMenus) {
         System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
         System.out.println("<주문 메뉴>");
         for (Menu menu : orderMenus.keySet()) {
             System.out.println(menu.getName() + " " + orderMenus.get(menu) + "개");
         }
-        System.out.println();
     }
 
     public void printTotalPrice(int totalPrice) {
-        System.out.println("<할인 전 총주문 금액>");
+        System.out.println("\n<할인 전 총주문 금액>");
         System.out.printf("%,d원\n", totalPrice);
     }
 
@@ -36,13 +36,13 @@ public class OutputView {
             return;
         }
         for (Event event : benefitsDetails.keySet()) {
-            System.out.printf("%s: -%,d원\n", event.getName(), benefitsDetails.get(event));
+            System.out.printf("%s: %,d원\n", event.getName(), benefitsDetails.get(event) * MINUS);
         }
     }
 
     public void printTotalBenefitAmount(int totalBenefitAmount) {
         System.out.println("\n<총혜택 금액>");
-        System.out.printf("%,d원\n", totalBenefitAmount * -1);
+        System.out.printf("%,d원\n", totalBenefitAmount * MINUS);
     }
 
     public void printFinalTotalPrice(int finalTotalPrice) {
